81317cd7c9b90d8ae4d79faba190f3b5b35c5c10
MoneroLite <73478668+MoneroLite@users.noreply.github.com>
2022-10-03 17:45:32 +0500

    added redux manager

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+import { IStatus } from "./index";
 
 function App() {
+  const dispatch = useDispatch();
+  const cash = useSelector((state: IStatus) => state.cash);
+
+  const addMoney = (cash: any) => {
+    dispatch({ type: "ADD_MONEY", payload: cash });
+  };
+
+  const getMoney = (cash: any) => {
+    dispatch({ type: "GET_MONEY", payload: cash });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Баланс: {cash}</p>
+      <button onClick={() => addMoney(Number(prompt()))}>внести</button>
+      <button onClick={() => getMoney(Number(prompt()))}>снять</button>
     </div>
   );
 }
--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,19 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+export type IStatus = { cash: number };
+
+const defStatus: IStatus = {
+  cash: 0,
+};
+
+const reducer = (state = defStatus, action: any) => {
+  switch (action.type) {
+    case "ADD_MONEY":
+      return { ...state, cash: state.cash + action.payload };
+    case "GET_MONEY":
+      return { ...state, cash: state.cash - action.payload };
+
+    default:
+      return state;
+  }
+};
+//
+const store = createStore(reducer);
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
