b8eefa6e68198c487c900501dedbdde6eb264ffb
MoneroLite <73478668+MoneroLite@users.noreply.github.com>
2022-10-13 16:37:40 +0500

    added toolit for redux

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -4,24 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { IStatus } from "./index";
 
 function App() {
-  const dispatch = useDispatch();
-  const cash = useSelector((state: IStatus) => state.cash);
-
-  const addMoney = (cash: any) => {
-    dispatch({ type: "ADD_MONEY", payload: cash });
-  };
-
-  const getMoney = (cash: any) => {
-    dispatch({ type: "GET_MONEY", payload: cash });
-  };
-
-  return (
-    <div className="App">
-      <p>Баланс: {cash}</p>
-      <button onClick={() => addMoney(Number(prompt()))}>внести</button>
-      <button onClick={() => getMoney(Number(prompt()))}>снять</button>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
--- front/src/index.tsx
+++ front/src/index.tsx
@@ -2,28 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { createStore } from "redux";
 import { Provider } from "react-redux";
-
-export type IStatus = { cash: number };
-
-const defStatus: IStatus = {
-  cash: 0,
-};
-
-const reducer = (state = defStatus, action: any) => {
-  switch (action.type) {
-    case "ADD_MONEY":
-      return { ...state, cash: state.cash + action.payload };
-    case "GET_MONEY":
-      return { ...state, cash: state.cash - action.payload };
-
-    default:
-      return state;
-  }
-};
-
-const store = createStore(reducer);
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
